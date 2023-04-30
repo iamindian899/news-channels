eb61797d98cd7ac4008481b39fc16024cd8dc8b0
iamindian899 <iamindian899@gmail.com>
2023-04-30 14:30:43 +0500

    seventh

---
+++ Training_Establishment/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="Training_Establishment"),
+    path("inter-services-establishments/", views.Inter_Services_Establishments, name="Inter_Services_Establishments"),
+    path("army-establishments/", views.Army_Establishments, name="Army_Establishments"),
+    path("training-team/", views.Training_Team, name="Training_Team"),
+    path("rashtriya-military-schools/", views.Rashtriya_Military_Schools, name="Rashtriya_Military_Schools"),
+    path("ignou-army-education-project-iaep/", views.Army_Education_Project, name="Army_Education_Project"),
+]
---
+++ Training_Establishment/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    template_name="Training_Establishment/index.html"
+    context={
+    
+    }
+    return render(request, template_name, context)
+
+
+
+def Inter_Services_Establishments(request):
+    template_name="Training_Establishment/Inter_Services_Establishments.html"
+    context={
+    
+    }
+    return render(request, template_name, context)
+
+
+def Army_Establishments(request):
+    template_name="Training_Establishment/Army_Establishments.html"
+    context={
+    
+    }
+    return render(request, template_name, context)
+
+
+def Training_Team(request):
+    template_name="Training_Establishment/Training_Team.html"
+    context={
+    
+    }
+    return render(request, template_name, context)
+
+
+
+def Rashtriya_Military_Schools(request):
+    template_name="Training_Establishment/Rashtriya_Military_Schools.html"
+    context={
+    
+    }
+    return render(request, template_name, context)
+
+
+def Army_Education_Project(request):
+    template_name="Training_Establishment/Army_Education_Project.html"
+    context={
+    
+    }
+    return render(request, template_name, context)
